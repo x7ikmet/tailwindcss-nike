5549a9dc015f84b46f7e1a3413de0c7cff127ca7
HikmeT <hekmt111@gmail.com>
2024-08-19 14:02:24 +0300

    Create index.js

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PouplarProduct from "./PouplarProduct";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+import SpecialOffers from "./SpecialOffers";
+import CustomerReviwes from "./CustomerReviwes";
+import Subscribe from "./Subscribe";
+import Footer from "./Footer";
+
+export {
+	Hero,
+	PouplarProduct,
+	SuperQuality,
+	Services,
+	SpecialOffers,
+	CustomerReviwes,
+	Subscribe,
+	Footer,
+};
